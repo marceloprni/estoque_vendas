c0188cc7bf78cd7672fbfa51e81b93405ff818a3
Marcelo Perini <marcelo.perini@wcvengenharia.com.br>
2025-03-12 07:43:31 -0300

    address modificated

--- src/address/address.service.ts
+++ src/address/address.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { AddressEntity } from './entities/address.entity';
 import { Repository } from 'typeorm';
